cf3432b11c54f5ba81788e029c0bb1ebd2a68ab4
roshaans2 <roshaansathya@gmail.com>
2023-09-25 11:30:53 +0530

    Added animation for correct and wrong answers

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -5,7 +5,7 @@ export default function Trivia(){
             <div className="answers">
                 <div className="answer">Rajinikanth</div>
                 <div className="answer">Kamal Hasan</div>
-                <div className="answer">Vijay</div>
+                <div className="answer wrong">Vijay</div>
                 <div className="answer">Ajith</div>
             </div>
         </div>
